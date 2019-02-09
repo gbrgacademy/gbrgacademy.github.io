6f053febf2f33500145c567480fbfc4732a4f576
Budh Ram Gurung <budhram.gurung01@gmail.com>
2019-02-09 17:26:52 +0530

    Migrated templates assets files

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,1 +1,39 @@
-/*Custom JavaScript*/
+// Your custom JavaScripts
+
+$(function(){
+  $('.counter').countUp();
+
+  $("#slider3").responsiveSlides({
+    auto: true,
+    pager: true,
+    nav: false,
+    speed: 500,
+    namespace: "callbacks",
+    before: function () {
+      $('.events').append("<li>before event fired.</li>");
+    },
+    after: function () {
+      $('.events').append("<li>after event fired.</li>");
+    }
+  });
+
+  $(".scroll").click(function (event) {
+    event.preventDefault();
+    $('html,body').animate({
+      scrollTop: $(this.hash).offset().top
+    }, 1000);
+  });
+
+  $().UItoTop({
+    easingType: 'easeOutQuart'
+  });
+});
+
+$(window).load(function () {
+  $('.flexslider').flexslider({
+    animation: "slide",
+    start: function (slider) {
+      $('body').removeClass('loading');
+    }
+  });
+});
